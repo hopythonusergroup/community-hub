508898d7648f39f48cf5b1582ef42ad4e5743332
Kafui Alordo <kalordo7@gmail.com>
2025-02-25 10:05:25 +0000

    add blog detail view

--- app/urls.py
+++ app/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from app.views import blog_list
+from app.views import blog_list, blog_detail
 
 urlpatterns = [
     path("", blog_list, name="blog_list"),
+    path("<int:post_id>/", blog_detail, name="blog_detail"),
 ]
--- app/views.py
+++ app/views.py
@@ -7,3 +7,8 @@ from app.models import BlogPost
 def blog_list(request):
     posts = BlogPost.objects.all().order_by("-created_at")
     return render(request, "app/blog_list.html", {"posts": posts})
+
+
+def blog_detail(request, post_id):
+    post = BlogPost.objects.filter(status="published").get(id=post_id)
+    return render(request, "app/blog_detail.html", {"post": post})
